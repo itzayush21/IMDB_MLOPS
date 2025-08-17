0bd3a6f6bb3e94b3814c6561576271391e48bf9d
itzayush21 <itzayush21@gmail.com>
2025-08-17 18:13:37 +0530

    application building

---
+++ src/connections/s3_connection.py
@@ -0,0 +1,58 @@
+import boto3
+import pandas as pd
+import logging
+from src.logger import logging
+from io import StringIO
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+class s3_operations:
+    def __init__(self):
+        """
+        Initialize the s3_operations class with AWS credentials and S3 bucket details
+        loaded from environment variables.
+        """
+        self.bucket_name = os.getenv("S3_BUCKET_NAME")
+        aws_access_key = os.getenv("AWS_ACCESS_KEY")
+        aws_secret_key = os.getenv("AWS_SECRET_KEY")
+        region_name = os.getenv("AWS_REGION", "us-east-1")
+
+        self.s3_client = boto3.client(
+            's3',
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key,
+            region_name=region_name
+        )
+        logging.info("✅ S3 client initialized successfully")
+
+    def fetch_file_from_s3(self, file_key):
+        """
+        Fetches a CSV file from the S3 bucket and returns it as a Pandas DataFrame.
+        :param file_key: S3 file path (e.g., 'data/data.csv')
+        :return: Pandas DataFrame
+        """
+        try:
+            logging.info(f"⬇️ Fetching file '{file_key}' from S3 bucket '{self.bucket_name}'...")
+            obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=file_key)
+            df = pd.read_csv(StringIO(obj['Body'].read().decode('utf-8')))
+            logging.info(f"✅ Successfully fetched '{file_key}' with {len(df)} records")
+            return df
+        except Exception as e:
+            logging.exception(f"❌ Failed to fetch '{file_key}' from S3: {e}")
+            return None
+
+
+
+
+# Example usage
+# if __name__ == "__main__":
+
+
+#     data_ingestion = s3_operations(BUCKET_NAME, AWS_ACCESS_KEY, AWS_SECRET_KEY)
+#     df = data_ingestion.fetch_file_from_s3(FILE_KEY)
+
+#     if df is not None:
+#         print(f"Data fetched with {len(df)} records..")  # Display first few rows of the fetched DataFrame
